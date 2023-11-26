a804e470953e00c979cf9de39fbb3307536be84d
Lumi <lucien.mou.pro@gmail.com>
2023-11-26 23:59:43 +0100

    👌 IMPROVE: upgrade

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <h2>About</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus vel, ut optio neque, nobis qui officia consequatur
-      corrupti eum in iusto reiciendis asperiores sequi quisquam fugit quos ducimus? Illum, provident?</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus vel, ut
+      optio neque, nobis qui officia consequatur corrupti eum in iusto
+      reiciendis asperiores sequi quisquam fugit quos ducimus? Illum, provident?
+    </p>
     <p>API response:</p>
     <div>{{ data }}</div>
   </div>
 </template>
 
 <script setup>
-  // const { data } = await useFetch('/api/ninja?name=mario')
-  // const { data } = await useFetch('/api/ninja?name=mario', {
-  //   method: 'post',
-  //   body: {age: 30}
-  // })
-const response = await useFetch('/api/currency/GBP')
+const appConfig = useAppConfig();
+// const { data } = await useFetch('/api/ninja?name=mario')
+// const { data } = await useFetch('/api/ninja?name=mario', {
+//   method: 'post',
+//   body: {age: 30}
+// })
+const response = await useFetch("/api/currency/GBP");
 const data = response.data;
 </script>
 
@@ -26,4 +30,5 @@ h2 {
 
 p {
   margin: 20px 0;
-}</style>
+}
+</style>
