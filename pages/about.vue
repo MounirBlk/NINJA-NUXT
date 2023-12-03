0975fee86880a0f0990f4c214b1cbf27f194934c
Lumi <lucien.mou.pro@gmail.com>
2023-12-03 20:45:17 +0100

    📦 NEW: PINIA

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h2>About</h2>
+    <h2>
+      About {{ store.count }} / {{ store.getCount }}
+      <button type="button" @click="increment">+</button>
+    </h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus vel, ut
       optio neque, nobis qui officia consequatur corrupti eum in iusto
@@ -12,6 +15,18 @@
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+
+const { increment } = store
+
+const count = computed(() => store.count)
+// const getCount = computed(() => store.getCount)
+
+// const { count } = storeToRefs(store)
+// const { getCount } = storeToRefs(store)
+
 const appConfig = useAppConfig()
 // const { data } = await useFetch('/api/ninja?name=mario')
 // const { data } = await useFetch('/api/ninja?name=mario', {
