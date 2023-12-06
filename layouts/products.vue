fab911fd8800f4327e0eba9418a2e5aa1a243263
Lumi <lucien.mou.pro@gmail.com>
2023-12-07 00:04:50 +0100

    🚀 RELEASE: UPGRADE

--- layouts/products.vue
+++ layouts/products.vue
@@ -2,7 +2,7 @@
   <div>
     <header class="shadow-sm bg-white">
       <nav class="container mx-auto p-4">
-        <NuxtLink to="/" class="font-bold">HOME</NuxtLink>
+        <NuxtLink to="/" class="font-bold">HOME PRODUCTS</NuxtLink>
         <v-spacer></v-spacer>
         <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
       </nav>
